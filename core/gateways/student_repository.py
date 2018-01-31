98f589e1ac849e1e9e02d98ed9761db60f6cc108
Miguel Lopez <miguel@devlo.io>
2018-01-31 14:08:54 -0700

    adding 1 more repo

---
+++ core/gateways/student_repository.py
@@ -0,0 +1,19 @@
+from core.interfaces.person_interface import PersonInterface
+from core.entities.student import Student
+from datetime import datetime, timedelta
+
+future_date = datetime.now() + timedelta(days=1)
+
+class StudentRepository(PersonInterface):
+
+    def __init__(self):
+        super().__init__()
+
+    # In-Memory Data
+    _student = Student
+
+    def get_by_id(self, personal_id):
+        return self._student
+
+    def save(self, person):
+        self._student = person
