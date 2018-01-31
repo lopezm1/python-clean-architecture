a4b76651bcb544e377bf6abfd9b51095b478968c
Miguel Lopez <miguel@devlo.io>
2018-01-31 13:35:26 -0700

    improve comments

--- core/interactors/course_registration_interactor.py
+++ core/interactors/course_registration_interactor.py
@@ -19,14 +19,16 @@ class CourseRegistrationInteractor(Interactor):
         if(self._auth_service.is_logged_in()):
             return res.ResponseFailure.build_authentication_error("Access denied, User not authenticated.")
 
-        # Get person
+        # Get person object
         person = self._person_repo.get_by_id(personal_id=request_object['personal_id'])
 
         errors = []
 
         for code in request_object['course_codes']:
+            # Get course object
             course = self._course_repo.get_by_code(code=code)
 
+            # Attempt to register for course
             if(not person.register_for_course(course)):
                 errors.append("Unable to register for " + code)
 
